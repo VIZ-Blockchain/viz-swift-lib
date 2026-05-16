40a00e72b18edbacbcdee883da9581ec18ea44c1
Vladimir Babin <vovababin@gmail.com>
2026-05-16 16:51:31 +0800

    test: add Asset string-parsing, precision, and decode-error coverage

--- Tests/UnitTests/Asset.swift
+++ Tests/UnitTests/Asset.swift
@@ -32,4 +32,65 @@ class AssetTest: XCTestCase {
         AssertDecodes(string: "0.001 VIZ", Asset(0.001, .viz))
         AssertDecodes(string: "123456789.999999 VESTS", Asset(123_456_789.999999, .vests))
     }
+
+    // MARK: - String parsing edge cases
+
+    func testMalformedStringsReturnNil() {
+        XCTAssertNil(Asset(""))
+        XCTAssertNil(Asset("VIZ"))
+        XCTAssertNil(Asset("10.000"))
+        XCTAssertNil(Asset("10.000VIZ"))
+        XCTAssertNil(Asset("abc VIZ"))
+        XCTAssertNil(Asset("10..0 VIZ"))
+    }
+
+    func testCustomSymbolPrecisionInference() {
+        let a = Asset("10.123456 FOO")
+        XCTAssertEqual(a?.symbol, .custom(name: "FOO", precision: 6))
+
+        let b = Asset("10 FOO")
+        XCTAssertEqual(b?.symbol, .custom(name: "FOO", precision: 0))
+
+        let c = Asset("10. FOO")
+        XCTAssertEqual(c?.symbol, .custom(name: "FOO", precision: 0))
+    }
+
+    func testNegativeAndZero() {
+        let neg = Asset(-1.5, .viz)
+        XCTAssertEqual(neg.description, "-1.500 VIZ")
+        XCTAssertEqual(neg.resolvedAmount, -1.5)
+
+        let zero = Asset(0, .viz)
+        XCTAssertEqual(zero.description, "0.000 VIZ")
+        XCTAssertEqual(zero.resolvedAmount, 0.0)
+    }
+
+    func testDescriptionExactPrecision() {
+        XCTAssertEqual(Asset(1, .viz).description, "1.000 VIZ")
+        XCTAssertEqual(Asset(1, .vests).description, "1.000000 VESTS")
+        XCTAssertEqual(Asset(1, .custom(name: "FOO", precision: 2)).description, "1.00 FOO")
+    }
+
+    // MARK: - Binary encoding for non-VIZ symbols
+
+    func testEncodeVestsBinary() {
+        AssertEncodes(Asset(1, .vests), Data("40420f00000000000656455354530000"))
+    }
+
+    func testEncodeCustomShortSymbolBinary() {
+        AssertEncodes(Asset(1, .custom(name: "FOO", precision: 2)), Data("640000000000000002464f4f00000000"))
+    }
+
+    // MARK: - Decode failure
+
+    func testDecodeFailsOnMalformedString() {
+        do {
+            _ = try TestDecode(Asset.self, string: "not an asset")
+            XCTFail("Expected DecodingError")
+        } catch is DecodingError {
+            // expected
+        } catch {
+            XCTFail("Unexpected error: \(error)")
+        }
+    }
 }

7e0136d12c85fcdd478a4e7fab30aecf0af13a2c
Vladimir Babin <vovababin@gmail.com>
2026-05-16 16:57:04 +0800

    test: update XCTestManifests for new unit tests and fix SeemURLTest typo
    The typo on the affected line meant VIZURLTest was silently skipped
    on Linux. Fix restores those 2 tests to Linux CI.

--- Tests/UnitTests/XCTestManifests.swift
+++ Tests/UnitTests/XCTestManifests.swift
@@ -2,8 +2,17 @@ import XCTest
 
 extension AssetTest {
     static let __allTests = [
+        ("testCustomSymbolPrecisionInference", testCustomSymbolPrecisionInference),
         ("testDecodable", testDecodable),
+        ("testDecodeFailsOnMalformedString", testDecodeFailsOnMalformedString),
+        ("testDescriptionExactPrecision", testDescriptionExactPrecision),
         ("testEncodable", testEncodable),
+        ("testEncodeCustomShortSymbolBinary", testEncodeCustomShortSymbolBinary),
+        ("testEncodeVestsBinary", testEncodeVestsBinary),
+        ("testEquateable", testEquateable),
+        ("testMalformedStringsReturnNil", testMalformedStringsReturnNil),
+        ("testNegativeAndZero", testNegativeAndZero),
+        ("testProperties", testProperties),
     ]
 }
 
@@ -34,6 +43,46 @@ extension OperationTest {
     static let __allTests = [
         ("testDecodable", testDecodable),
         ("testEncodable", testEncodable),
+        ("testEncodeAsymmetry_definedButNotEncodable", testEncodeAsymmetry_definedButNotEncodable),
+        ("testRoundTrip_accountCreate", testRoundTrip_accountCreate),
+        ("testRoundTrip_accountUpdate", testRoundTrip_accountUpdate),
+        ("testRoundTrip_accountWitnessProxy", testRoundTrip_accountWitnessProxy),
+        ("testRoundTrip_accountWitnessVote", testRoundTrip_accountWitnessVote),
+        ("testRoundTrip_award", testRoundTrip_award),
+        ("testRoundTrip_benefactorAward", testRoundTrip_benefactorAward),
+        ("testRoundTrip_changeRecoveryAccount", testRoundTrip_changeRecoveryAccount),
+        ("testRoundTrip_content", testRoundTrip_content),
+        ("testRoundTrip_delegateVestingShares", testRoundTrip_delegateVestingShares),
+        ("testRoundTrip_escrowApprove", testRoundTrip_escrowApprove),
+        ("testRoundTrip_escrowDispute", testRoundTrip_escrowDispute),
+        ("testRoundTrip_escrowRelease", testRoundTrip_escrowRelease),
+        ("testRoundTrip_escrowTransfer", testRoundTrip_escrowTransfer),
+        ("testRoundTrip_inviteRegistration", testRoundTrip_inviteRegistration),
+        ("testRoundTrip_receiveAward", testRoundTrip_receiveAward),
+        ("testRoundTrip_recoverAccount", testRoundTrip_recoverAccount),
+        ("testRoundTrip_requestAccountRecovery", testRoundTrip_requestAccountRecovery),
+        ("testRoundTrip_setWithdrawVestingRoute", testRoundTrip_setWithdrawVestingRoute),
+        ("testRoundTrip_transfer", testRoundTrip_transfer),
+        ("testRoundTrip_transferToVesting", testRoundTrip_transferToVesting),
+        ("testRoundTrip_vote", testRoundTrip_vote),
+        ("testRoundTrip_withdrawVesting", testRoundTrip_withdrawVesting),
+        ("testRoundTrip_witnessUpdate", testRoundTrip_witnessUpdate),
+        ("testUnknownMappedOps_decodeAsUnknown", testUnknownMappedOps_decodeAsUnknown),
+        ("testVirtual", testVirtual),
+        ("testVirtualDecode_authorReward", testVirtualDecode_authorReward),
+        ("testVirtualDecode_commentPayoutUpdate", testVirtualDecode_commentPayoutUpdate),
+        ("testVirtualDecode_commentReward", testVirtualDecode_commentReward),
+        ("testVirtualDecode_curationReward", testVirtualDecode_curationReward),
+        ("testVirtualDecode_fillConvertRequest", testVirtualDecode_fillConvertRequest),
+        ("testVirtualDecode_fillOrder", testVirtualDecode_fillOrder),
+        ("testVirtualDecode_fillTransferFromSavings", testVirtualDecode_fillTransferFromSavings),
+        ("testVirtualDecode_fillVestingWithdraw", testVirtualDecode_fillVestingWithdraw),
+        ("testVirtualDecode_hardfork", testVirtualDecode_hardfork),
+        ("testVirtualDecode_interest", testVirtualDecode_interest),
+        ("testVirtualDecode_liquidityReward", testVirtualDecode_liquidityReward),
+        ("testVirtualDecode_returnVestingDelegation", testVirtualDecode_returnVestingDelegation),
+        ("testVirtualDecode_shutdownWitness", testVirtualDecode_shutdownWitness),
+        ("testVirtualDecode_witnessReward", testVirtualDecode_witnessReward),
     ]
 }
 
@@ -91,9 +140,15 @@ extension SignatureTest {
 extension VIZEncoderTest {
     static let __allTests = [
         ("testArray", testArray),
+        ("testBool", testBool),
+        ("testDate", testDate),
         ("testFixedWidthInteger", testFixedWidthInteger),
+        ("testLargeArrayVarintLength", testLargeArrayVarintLength),
+        ("testOptional", testOptional),
+        ("testRawData", testRawData),
         ("testSortedDict", testSortedDict),
         ("testString", testString),
+        ("testVarintBoundaries", testVarintBoundaries),
     ]
 }
 
@@ -115,7 +170,7 @@ extension TransactionTest {
             testCase(PrivateKeyTest.__allTests),
             testCase(PublicKeyTest.__allTests),
             testCase(Secp256k1Test.__allTests),
-            testCase(SeemURLTest.__allTests),
+            testCase(VIZURLTest.__allTests),
             testCase(Sha2Test.__allTests),
             testCase(SignatureTest.__allTests),
             testCase(VIZEncoderTest.__allTests),
